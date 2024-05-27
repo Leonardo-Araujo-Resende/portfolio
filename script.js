ed99e4ee6c58f26da85a23ebe7a1ef33b2a82413
Leonardo-Araujo-Resende <leonardo.a.aguiar@ufv.br>
2024-05-27 17:40:33 -0300

    rascunho

--- script.js
+++ script.js
@@ -3,67 +3,6 @@ const horas = document.querySelector("#hora .valor")
 const minutos = document.querySelector("#minuto .valor")
 const segundos = document.querySelector("#segundo .valor")
 
-//Styles cores
-const root = document.documentElement;
-
-
-const mode1 = document.querySelector("#mode1")
-const mode2 = document.querySelector("#mode2")
-const mode3 = document.querySelector("#mode3")
-const mode4 = document.querySelector("#mode4")  
-const mode5 = document.querySelector("#mode5")
-
-//Funcoes mudar cores
-mode1.addEventListener('click', function() {
-  const color1 = getComputedStyle(document.querySelector('#mode1')).backgroundColor;
-  const color2 = getComputedStyle(document.querySelector('#mode1')).borderColor;
-
-  root.style.setProperty('--cor1', color1); 
-  root.style.setProperty('--cor2', color2); 
-});
-mode2.addEventListener('click', function() {
-  const color1 = getComputedStyle(document.querySelector('#mode2')).backgroundColor;
-  const color2 = getComputedStyle(document.querySelector('#mode2')).borderColor;
-
-  root.style.setProperty('--cor1', color1); 
-  root.style.setProperty('--cor2', color2);  
-});
-mode3.addEventListener('click', function() {
-  const color1 = getComputedStyle(document.querySelector('#mode3')).backgroundColor;
-  const color2 = getComputedStyle(document.querySelector('#mode3')).borderColor;
-
-  root.style.setProperty('--cor1', color1); 
-  root.style.setProperty('--cor2', color2); 
-});
-mode4.addEventListener('click', function() {
-  const color1 = getComputedStyle(document.querySelector('#mode4')).backgroundColor;
-  const color2 = getComputedStyle(document.querySelector('#mode4')).borderColor;
-
-  root.style.setProperty('--cor1', color1); 
-  root.style.setProperty('--cor2', color2); 
-});
-mode5.addEventListener('click', function() {
-  const color1 = getComputedStyle(document.querySelector('#mode5')).backgroundColor;
-  const color2 = getComputedStyle(document.querySelector('#mode5')).borderColor;
-
-  root.style.setProperty('--cor1', color1); 
-  root.style.setProperty('--cor2', color2); 
-});
-
-//Funcao inverter cores
-botaoInverter = document.querySelector("#inverter");
-
-  botaoInverter.addEventListener("click", function() {
-
-      const cor1 = getComputedStyle(document.documentElement).getPropertyValue('--cor1');
-      const cor2 = getComputedStyle(document.documentElement).getPropertyValue('--cor2');
-
-      document.documentElement.style.setProperty('--cor1', cor2);
-      document.documentElement.style.setProperty('--cor2', cor1);
-  });
-
-
-
 
 const relogio = setInterval(function time(){
 
