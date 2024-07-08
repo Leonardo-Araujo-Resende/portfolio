5dcdc8b8c906a267b1c3f89c99450799b327fe53
Leonardo-Araujo-Resende <leonardo.a.aguiar@ufv.br>
2024-07-08 17:41:27 -0300

    Costuras

--- script.js
+++ script.js
@@ -4,11 +4,18 @@ const paginaSobreMim = document.querySelector('#pagina-sobre-mim');
 const paginaHabilidades = document.querySelector('#pagina-habilidades');
 const paginaProjetos = document.querySelector('#pagina-projetos');
 
+//Botoes projeto
+const botaoFacelita = document.querySelector("#botao-facelita");
+
 //Divs
 const divHome = document.querySelector('#home');
 const divSobreMim = document.querySelector('#sobre-mim');
 const divHabilidades = document.querySelector('#habilidades');
 const divProjetos = document.querySelector('#projetos');
+
+//Popup
+const popupsDiv = document.querySelector("#popups");
+const popupFacelita = document.querySelector(".popup-facelita");
 
 paginaHome.addEventListener("click", function(){
     divHome.scrollIntoView({ behavior: 'smooth' });
@@ -41,3 +48,26 @@ setInterval(() => {
   }
 
 }, 600); 
+
+
+function ativarPopup(){
+  popupsDiv.classList.remove("escondido");
+}
+
+function desativarPopup(){
+  popupsDiv.classList.add("escondido");
+}
+
+function ativarFaceLita(){
+  ativarPopup();
+  popupFacelita.classList.remove("escondido");
+}
+
+function desativarFaceLita(){
+  desativarPopup();
+  popupFacelita.classList.add("escondido");
+}
+
+botaoFacelita.addEventListener("click",function(){
+  ativarFaceLita();
+})
